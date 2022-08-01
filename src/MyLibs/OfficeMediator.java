14f5893775970e9e74f9c6ae243058aa1257000d
IzakVillanueva <82789169+IzakVillanueva@users.noreply.github.com>
2022-08-02 01:01:15 +0800

    Create office mediator

---
+++ src/MyLibs/OfficeMediator.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MyLibs;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Izak
+ */
+public class OfficeMediator implements IMediator{
+    
+    public OfficeMediator(){
+    }
+    
+    @Override
+    public void addEquipment(Office office, Equipment equipment) {
+        
+    }
+
+    @Override
+    public void removeEquipment(Office office, Equipment equipment) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void moveEquipment(Office office1, Office office2, Equipment equipment) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
