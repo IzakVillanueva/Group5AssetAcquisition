c97fe7ea91b9f490f24e411f75d0612ad5aeaccb
IzakVillanueva <82789169+IzakVillanueva@users.noreply.github.com>
2022-08-02 02:09:54 +0800

    Update office methods

--- src/MyLibs/Office.java
+++ src/MyLibs/Office.java
@@ -15,9 +15,12 @@ public abstract class Office {
     public String name;
     public int officeHoursStart, officeHoursEnd, squareFeet;
     public double height, capacity;
-    public ArrayList <Equipment> equipments; 
+    public ArrayList<Equipment> equipments = new ArrayList<Equipment>(); 
+    private OfficeMediator mediator;
     
-    
+    public Office(OfficeMediator mediator){
+        this.mediator = mediator;
+    }
     
     public void openOffice()
     {
@@ -27,5 +30,19 @@ public abstract class Office {
     public void closeOffice()
     {
         
+    }
+    
+    public void addEquipment(Office office, Equipment equipment) {
+        mediator.addEquipment(office, equipment);
+        //this.equipments.add(equipment);
+        //equipment.office = equipment.name;
+    }
+
+    public void removeEquipment(Office office, Equipment equipment) {
+        mediator.removeEquipment(office, equipment);
+    }
+
+    public void moveEquipment(Office office1, Office office2, Equipment equipment) {
+        mediator.moveEquipment(office1, office2, equipment);
     }
 }
